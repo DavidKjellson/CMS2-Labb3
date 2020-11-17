4715e2df58260b845e5eb22946b228c6f5e459c4
David Kjellson <david.kjellson@gmail.com>
2020-11-17 11:20:50 +0100

    Info on plugin #1

---
+++ vardedata/vardedata.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Plugin Name: Värdedata
+ * Description: Ett plugin som hämtar väderdata för vald plats, som presenteras med lämpliga bilder och färger.
+ * Version: 1.0
+ * Author: David Kjellson
+ **/
