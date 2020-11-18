bd4ab16e8f8df49934801effe80097ab0dc0f2b3
David Kjellson <david.kjellson@gmail.com>
2020-11-18 15:03:22 +0100

    Added an allright weather API.

--- vaderdata/vaderdata.php
+++ vaderdata/vaderdata.php
@@ -6,10 +6,19 @@
  * Version: 1.0
  * Author: David Kjellson
  **/
-
-function test()
+function get_weather()
 {
-  echo "Test!";
+  include 'secret.php';
+  $url = 'http://api.openweathermap.org/data/2.5/weather?q=Gothenburg,Sweden&APPID=' . $API_KEY;
+  $response = wp_remote_get($url);
+  if (is_array($response)) {
+    $header = $response['headers'];
+    $body = $response['body'];
+    $resp = json_decode($body);
+    $celsius = $resp->main->temp - 273.15;
+    echo 'Vädret i Göteborg är: ' . $celsius . '°.';
+    echo '<img src="http://openweathermap.org/img/wn/' . $resp->weather[0]->icon . '@2x.png">';
+  }
 }
 
-add_action('woocommerce_single_product_summary', 'test');
+add_action('woocommerce_single_product_summary', 'get_weather');
