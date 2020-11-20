a16c263d049c737a5673a01407a296e538e9abc1
David Kjellson <david.kjellson@gmail.com>
2020-11-20 15:16:42 +0100

    OOP'd

--- kontaktformular/kontaktformular.php
+++ kontaktformular/kontaktformular.php
@@ -6,60 +6,67 @@
  * Version: 1.0
  * Author: David Kjellson
  **/
-
-function contact()
-{ ?>
-  <div class="container">
-    <form action="<?php echo admin_url('admin-ajax.php'); ?>">
-      <input type="text" name="namn">
-      <input type="submit" value="Skicka">
-      <input type="hidden" name="action" value="dk_contactform">
-    </form>
-  </div>
+class Contact
+{
+  function contact()
+  { ?>
+    <div class="container">
+      <form action="<?php echo admin_url('admin-ajax.php'); ?>">
+        <input type="text" name="namn">
+        <input type="email" name="email">
+        <input type="text" name="meddelande">
+        <input type="submit" value="Skicka">
+        <input type="hidden" name="action" value="dk_contactform">
+      </form>
+    </div>
 <?php }
 
-function receivemessage()
-{
-  echo 'Tack för ditt meddelande ' . $_REQUEST['namn'] . '!';
-  die();
-  // die($_REQUEST['namn']);
+  function receivemessage()
+  {
+    echo 'Tack för ditt meddelande ' . $_REQUEST['namn'] . '!';
+    die();
+  }
+
+  function insertpost()
+  {
+    // Skapa en post  
+    // Namn, innehåll, avsändarens epost
+    $post_id = wp_insert_post(
+      [
+        'post_title' => $_REQUEST['namn'],
+        'post_content' => $_REQUEST['meddelande'],
+        'post_type' => 'meddelanden'
+      ]
+    );
+    update_post_meta($post_id, 'email', $_REQUEST['email']);
+  }
+  public function __construct()
+  {
+    add_action('wp_ajax_dk_contactform', [$this, 'receivemessage']);
+    add_action('init', [$this, 'messages']);
+    add_action('', [$this, 'insertpost']);
+    add_action('woocommerce_after_main_content', [$this, 'contact']);
+  }
+
+  // Creates post types of messages.
+  function messages()
+  {
+    register_post_type('meddelanden', [
+      'labels' => [
+        'name' => 'Meddelanden',
+        'singular_name' => 'Meddelande'
+      ],
+      'public' => true,
+      'has_archive' => true
+    ]);
+  }
 }
-
-function insertpost()
-{
-  // Skapa en post  
-  // Namn, innehåll, avsändarens epost
-  wp_insert_post(
-    [
-      'post_title' => 'Dave',
-      'post_content' => 'Jag är bäst!!',
-      'post_type' => 'meddelanden'
-    ]
-  );
-}
-
-add_action('wp_ajax_dk_contactform', 'receivemessage');
-
-// Creates post types of messages.
-function messages()
-{
-  register_post_type('meddelanden', [
-    'labels' => [
-      'name' => 'Meddelanden',
-      'singular_name' => 'Meddelande'
-    ],
-    'public' => true,
-    'has_archive' => true
-  ]);
-}
-
-add_action('init', 'messages');
-add_action('', 'insertpost');
-// do_action('save_post_{$post->messages}');
-add_action('woocommerce_after_main_content', 'contact'); ?>
+$contact = new Contact();
+?>
 
 <style>
   input[type=text],
+  input[type=email],
   select,
   textarea {
     width: 100%;
