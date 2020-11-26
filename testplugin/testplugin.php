d0f2a7bc3c603fd8cce4b82d774412a62f393c44
David Kjellson <david.kjellson@gmail.com>
2020-11-26 15:36:26 +0100

    testfunction and runtest functions

--- testplugin/testplugin.php
+++ testplugin/testplugin.php
@@ -7,4 +7,32 @@
  * Author: David Kjellson
  **/
 
-$testcounter = new Counter();
+class TestCounter
+{
+  function testfunction($strangen_som_ska_testas, $forvantat_returvarde)
+  {
+    $testcount = new Counter();
+    $isworking = $testcount->is_seven_letters_long($strangen_som_ska_testas);
+    if ($isworking === $forvantat_returvarde) {
+      echo '<p style="color: green;">Lyckat test!</p>';
+    } else {
+      echo '<p style="color: red;">Test misslyckades...</p>';
+    }
+  }
+
+  function runtest()
+  {
+    $six = '123456';
+    $seven = '1234567';
+    $nine = '123456789';
+    $this->testfunction($seven, true);
+    $this->testfunction($six, false);
+    $this->testfunction($nine, false);
+  }
+
+  public function __construct()
+  {
+    add_action('init', [$this, 'runtest']);
+  }
+}
+$test = new TestCounter();
